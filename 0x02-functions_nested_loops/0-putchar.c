03755cec6d3edd04c819ee34288286ae53b52caf
AliciaPanashe <alicipanashe@gmail.com>
2023-02-21 14:49:31 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * Description: using the main function
+ * this prints _putchar followoed by a new line
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
