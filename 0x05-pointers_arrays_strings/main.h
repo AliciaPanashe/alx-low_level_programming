c49bc8b243e58b78a16671ecd8cfaa071355d51f
AliciaPanashe <alicipanashe@gmail.com>
2023-02-27 09:23:21 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-#include <stdio.h>
-#include <unistd.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ */
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +16,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
 
-#endif
+#endif /* MIAN_H_INCLUDED */
