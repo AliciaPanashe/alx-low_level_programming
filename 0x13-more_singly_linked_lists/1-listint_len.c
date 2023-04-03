7e14657d81a232ee68975ab1e44e0595e5bea48d
AliciaPanashe <aliciapanashe@gmail.com>
2023-04-03 10:50:01 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,12 @@
 
 /**
  * listint_len - Returns the number of elements
- * in a linked listint_t list.
+ *    in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of the elements in the listint_t list.
  */
 size_t listint_len(const listint_t *h);
-
 {
 	size_t num = 0;
 
